b20fc15fe3e432b7683caa1c396ca275ddb15f38
O-Galaszkiewicz <156899800+O-Galaszkiewicz@users.noreply.github.com>
2024-01-31 19:12:32 +0000

    Create lines.rkt

---
+++ lines.rkt
@@ -0,0 +1,16 @@
+;Northern Line:
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2021/09/nor_big-plus-2021-extension-scaled.gif
+  ;High Barnet -> Totteridge & Whetstone = 4
+  ;Totteridge & Whetstone -> Woodside Park = 2
+  ;Woodside Park -> West Finchley = 2
+  ;West Finchley -> Finchley Central = 2
+  ;Finchley Central -> Mill Hill East = 3
+  ;Finchley Central -> East Finchley = 4
+  ;East Finchley -> Highgate = 2
+  ;Highgate -> Archway = 3
+  ;Archway -> Tufnell Park = 2
+  ;Tufnell Park -> Kentish Town = 2
+  ;Kentish Town -> Camden Town = 2
+  ;
+  ;
+  ;
