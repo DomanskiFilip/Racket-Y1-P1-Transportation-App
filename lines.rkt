bc30355b4fdd582cb02bc1649d99c133cba36302
O-Galaszkiewicz <156899800+O-Galaszkiewicz@users.noreply.github.com>
2024-01-31 22:22:08 +0000

    Update lines.rkt
    Minor Changes

--- lines.rkt
+++ lines.rkt
@@ -1,5 +1,9 @@
-;Northern Line:
+;In-app the peak times should vary in the range of +/- 2 (if we get to the part of the app randomizing events to challenge the app - railworks, strikes, stations closing etc.)
+;Add the details to the lines we will include and remove the ones we dont need/want
+
+;Northern Line (Off-peak Times):
   ;Image: https://lasttrain.co.uk/wp-content/uploads/2021/09/nor_big-plus-2021-extension-scaled.gif
+  ;Station From -> Station To = Time
   ;High Barnet -> Totteridge & Whetstone = 4
   ;Totteridge & Whetstone -> Woodside Park = 2
   ;Woodside Park -> West Finchley = 2
@@ -11,6 +15,81 @@
   ;Archway -> Tufnell Park = 2
   ;Tufnell Park -> Kentish Town = 2
   ;Kentish Town -> Camden Town = 2
-  ;
-  ;
-  ;
+  ;Camden Town -> Chalk Farm = 2
+  ;Chalk Farm -> Belsize Park = 2
+  ;Belsize Park -> Hampstead = 2
+  ;Hampstead -> Golders Green = 4
+  ;Golders Green -> Brent Cross = 3
+  ;Brent Cross -> Hendon Central = 2
+  ;Hendon Central -> Colindale = 3
+  ;Colindale -> Burnt Oak = 2
+  ;Burnt Oak -> Edgeware = 3
+  ;Camden Town -> Euston = 3
+  ;Camden Town -> Mornington Crescent = 2
+  ;Mornington Crescent -> Euston = 1
+  ;Euston -> Kings Cross St Pancras = 2
+  ;Kings Cross St Pancras -> Angel = 2
+  ;Angel -> Old Street = 3
+  ;Old Street -> Moorgate = 2
+  ;Moorgate -> Bank = 2
+  ;Bank -> London Bridge = 2
+  ;London Bridge -> Borough = 1
+  ;Borough -> Elephant & Castle = 2
+  ;Elephant & Castle -> Kennington = 2
+  ;Euston -> Warren Street = 1
+  ;Warren Street -> Goodge Street = 2
+  ;Goodge Street -> Tottenham Court Road = 1
+  ;Tottenham Court Road -> Leicester Square = 1
+  ;Leicester Square -> Charing Cross = 2
+  ;Charing Cross -> Embankment = 1
+  ;Embankment -> Waterloo = 2
+  ;Waterloo -> Kennington = 3
+  ;Kennington -> Nine Elms = ? (assuming 2)
+  ;Nine Elms -> Battersea Power Station = ? (assuming 2)
+  ;Kennington -> Oval = 2
+  ;Oval -> Stockwell = 2
+  ;Stockwell -> Clapham North = 2
+  ;Clapham North -> Clapham Common = 1
+  ;Clapham Common -> Clapham South = 2
+  ;Clapham South -> Balham = 2
+  ;Balham -> Tooting Bec = 2
+  ;Tooting Bec -> Tooting Broadway = 2
+  ;Tooting Broadway -> Colliers Wood = 2
+  ;Colliers Wood -> South Wimbledon = 2
+  ;South Wimbledon -> Morden = 3
+
+;Bakerloo Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/bak_big-1.gif
+
+;Central Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/cen_big-1.gif
+
+;Circle Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/cir_big.gif
+
+;District Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/dis_big.gif
+
+;Elizabeth Line
+  ;Image: https://lasttrain.co.uk/wp-content/webpc-passthru.php?src=https://lasttrain.co.uk/wp-content/uploads/2022/05/elizabeth-line-map-2022.jpg&nocache=1
+
+;Hammersmith & City Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/cir_big.gif
+
+;Jubilee Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/jub_big.gif
+
+;Metropolitan Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/met_big.gif
+
+;Piccadilly Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/pic_big.gif
+
+;Victoria Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/vic_big.gif
+
+;Waterloo & City Line
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/wat_big.gif
+
+;London Overground
+  ;Image: https://lasttrain.co.uk/wp-content/uploads/2016/05/london-overground-network-map.gif
