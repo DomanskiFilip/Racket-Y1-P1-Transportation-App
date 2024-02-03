09d14dc497d159107f8a6c8b71832dd111131b54
O-Galaszkiewicz <ogalaszkiewicz@gmail.com>
2024-02-03 01:01:13 +0000

    Changes + New File (functions.rkt)
    for lines.txt, i removed the semi colons
    for GUI.rkt, i changed the indentation spaces
    
    functions.rkt has some functions which can be used for the app, also has the hash table for the stations and the times between them

--- GUI.rkt
+++ GUI.rkt
@@ -1,30 +1,30 @@
 #lang racket/gui
 (define myframe (new frame%
-       [label "World's best travel app"]
-       [width 250]
-       [height 100]))
+    [label "World's best travel app"]
+    [width 250]
+    [height 100]))
 
 (define start (new text-field%
-       [label "            Start: "]
-       [parent myframe]
-       [style '(single)]
-       [enabled #t]))
+    [label "            Start: "]
+    [parent myframe]
+    [style '(single)]
+    [enabled #t]))
 
 (define destination (new text-field%
-       [label "Destination: "]
-       [parent myframe]
-       [style '(single)]
-       [enabled #t]))
+    [label "Destination: "]
+    [parent myframe]
+    [style '(single)]
+    [enabled #t]))
 
 (new button%
-     [label "Start Journey"]
-     [parent myframe]
-     [callback (lambda (a b)
-                 (send output set-value "idk :("))])
+    [label "Start Journey"]
+    [parent myframe]
+    [callback (lambda (a b)
+    (send output set-value "idk :("))])
 
 
 (define output(new text-field%
-                  [label "  Directions: "]
-                  [parent myframe]))
+    [label "  Directions: "]
+    [parent myframe]))
                  
 (send myframe show #t)
